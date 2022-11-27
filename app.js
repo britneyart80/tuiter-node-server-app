5a2b32cc9a1f3caf848917d9b887a9d3d0b550bb
britneyart80 <britneychen@britneys-mbp.mynetworksettings.com>
2022-11-27 10:51:55 -0500

    add mongo db

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ import cors from "cors";
 
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING =
+  "mongodb+srv://britney:dyK65b0z7UOPYdc0@cluster0.eaq3hdp.mongodb.net/?retryWrites=true&w=majority" ||
+  "mongodb://localhost:27017/tuiter";
+mongoose.connect(CONNECTION_STRING);
+mongoose.connect("mongodb://localhost:27017/tuiter");
 const app = express();
 app.use(express.json());
 app.use(cors());
